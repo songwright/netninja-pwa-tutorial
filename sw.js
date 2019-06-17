24e2e560b6fec3c250e42f409667aafcba0bd49c
Ramon Sanchez <songwright66@gmail.com>
2019-06-17 15:55:42 -0700

    feat: Add static cache & assets array to sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/js/app.js',
+  '/js/ui.js',
+  '/index.html',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
 // install service worker
 self.addEventListener('install', evt => {
-  console.log('Service worker has been installed');
+  // console.log('Service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('Caching shell assests');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate service worker
 self.addEventListener('activate', evt => {
-  console.log('Service worker has been activated')
+  // console.log('Service worker has been activated')
 })
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 })
