4bf1cc580eb8299446a1ec085526c131e1f398fc
Ramon Sanchez <songwright66@gmail.com>
2019-06-17 10:07:15 -0700

    feat: Update objects reg & err in sw registration

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
   navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log('Service worker registered.'))
-    .catch(() => console.log('Service worker not registered.'))
+    .then((reg) => console.log('Service worker registered.', reg))
+    .catch((err) => console.log('Service worker not registered.', err))
 }
