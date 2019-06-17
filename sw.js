7293ee1994852213a0d24344aa6033f66cd559c1
Ramon Sanchez <songwright66@gmail.com>
2019-06-17 12:41:40 -0700

    feat: Add activate listener to sw.js

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', evt => {
   console.log('Service worker has been installed');
 });
+
+// activate service worker
+self.addEventListener('activate', evt => {
+  console.log('Service worker has been activated')
+})
