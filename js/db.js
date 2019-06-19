6529f6fc0e5044319d569df4e6896e080dc3e150
Ramon Sanchez <songwright66@gmail.com>
2019-06-19 16:37:55 -0700

    feat: Enable offline data persistence

--- js/db.js
+++ js/db.js
@@ -1,3 +1,15 @@
+// Enable offline data
+db.enablePersistence()
+  .catch(err => {
+    if(err.code == 'failed-precondition'){
+      // probably multiple tabs open at once
+      console.log('Persistence failed.');
+    } else if(err.code == 'unimplemented'){
+      // lack of browser support
+      console.log('Persistence is not available.');
+    }
+  })
+
 // real-time listener
 db.collection('recipes').onSnapshot((snapshot) => {
   // console.log(snapshot.docChanges());
