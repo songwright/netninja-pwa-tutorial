de16c78886ef8e840756d210b85840bcdae62007
Ramon Sanchez <songwright66@gmail.com>
2019-06-19 13:38:05 -0700

    feat: Update real-time db listener

--- js/db.js
+++ js/db.js
@@ -1,4 +1,13 @@
 // real-time listener
 db.collection('recipes').onSnapshot((snapshot) => {
-  console.log(snapshot.docChanges());
+  // console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach(change => {
+    // console.log(change, change.doc.data(), change.doc.id);
+    if(change.type === 'added'){
+      // add the document data to the web page
+    }
+    if(change.type === 'removed'){
+      // remove the document from the web page
+    }
+  })
 });
