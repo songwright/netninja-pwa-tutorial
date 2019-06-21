0715843d4cc41ac4b2b9467e5abcfec7356486d7
Ramon Sanchez <songwright66@gmail.com>
2019-06-21 14:07:12 -0700

    feat: Comment out service worker console log

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
   navigator.serviceWorker.register('/sw.js')
-    .then((reg) => console.log('Service worker registered.', reg))
+    // .then((reg) => console.log('Service worker registered.', reg))
     .catch((err) => console.log('Service worker not registered.', err))
 }
