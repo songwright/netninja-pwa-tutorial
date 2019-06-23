2db097c43fa9715f7cc1fa47fe5c637174b9e165
Ramon Sanchez <songwright66@gmail.com>
2019-06-22 23:29:48 -0700

    feat: Update style and ui for ingredients

--- js/ui.js
+++ js/ui.js
@@ -16,7 +16,7 @@ const renderRecipe = (data, id) => {
       <img src="/img/dish.png" alt="recipe thumb">
       <div class="recipe-details">
         <div class="recipe-title">${data.title}</div>
-        <div class="recipe-ingredients">${data.ingredients}</div>
+        <div class="recipe-ingredients"><span>Ingredients:</span> ${data.ingredients}</div>
       </div>
       <div class="recipe-delete">
         <i class="material-icons" data-id="${id}">delete_outline</i>
