c083b7dcd54b733575543b104ca7b88dc0b55928
Ramon Sanchez <songwright66@gmail.com>
2019-08-30 12:21:51 -0700

    feat: Add logout function to auth.js file

--- scripts/auth.js
+++ scripts/auth.js
@@ -14,3 +14,10 @@ signupForm.addEventListener('submit', (e) => {
     signupForm.reset();
   })
 })
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut()
+})
