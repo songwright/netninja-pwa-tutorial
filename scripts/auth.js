63b48d93546ad528fb4f7d2244196d66309819e7
Ramon Sanchez <songwright66@gmail.com>
2019-08-30 10:00:04 -0700

    feat: Add signupForm event listener to auth.js

---
+++ scripts/auth.js
@@ -0,0 +1,16 @@
+// sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get user info.
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // Sign up the user.
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  })
+})
