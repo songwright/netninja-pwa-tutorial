79f92311b5b21ee2ba5095e407eb5191f6db42c5
Ramon Sanchez <songwright66@gmail.com>
2019-09-03 10:46:20 -0700

    feat: Update add recipe error message

--- js/db.js
+++ js/db.js
@@ -38,7 +38,7 @@ form.addEventListener('submit', evt => {
   };
 
   db.collection('recipes').add(recipe)
-    .catch(err => console.log(err))
+    .catch(err => console.log(err.message))
     .catch(alert('You must log in to create a recipe.'))
 
   form.title.value = '';
